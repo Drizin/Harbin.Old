86912f76536dfaa01cde77f727c793d90b920b82
Rick Drizin <Drizin@users.noreply.github.com>
2019-11-03 21:56:11 -0500

    Unit tests (with MSTest.TestFramework)

---
+++ src/AdventureWorks.Business.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("AdventureWorks.Business.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("AdventureWorks.Business.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("36365bcf-4575-428e-a017-8bd820e974d7")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ src/AdventureWorks.Business.Tests/UnitTest1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using AdventureWorksDB = AdventureWorks.Business.Entities.AdventureWorksDB;
+
+namespace AdventureWorks.Business.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var db = new AdventureWorksDB();
+            var person = db.People.First();
+            Assert.IsNotNull(person);
+            Assert.IsNotNull(person.FirstName);
+            var person2 = new AdventureWorksDB().Query<Entities.Person>("SELECT * FROM [Person].[Person] WHERE [BusinessEntityID]=@BusinessEntityID",
+                new { person.BusinessEntityId }).Single();
+            Assert.IsNotNull(person2);
+            Assert.AreEqual(person.FirstName, person2.FirstName);
+            Assert.AreEqual(person.BusinessEntityId, person2.BusinessEntityId);
+        }
+
+        [TestMethod]
+        public void TestInsert1()
+        {
+            var db = new AdventureWorksDB();
+            var person = db.People.First();
+            Assert.IsNotNull(person);
+            Assert.IsNotNull(person.BusinessEntityId);
+            person = new Entities.Person();
+            Assert.IsNull(person.BusinessEntityId);
+            person.PersonType = "GC";
+            person.NameStyle = false;
+            person.FirstName = "Rick";
+            person.LastName = "Drizin";
+            person.EmailPromotion = 0;
+            db.People.Add(person);
+            int savedRecords = db.SaveChanges();
+            Assert.AreEqual(savedRecords, 1);
+            Assert.IsNotNull(person.BusinessEntityId);
+
+            var person2 = new AdventureWorksDB().Query<Entities.Person>("SELECT * FROM [Person].[Person] WHERE [BusinessEntityID]=@BusinessEntityID",
+                new { person.BusinessEntityId }).Single();
+            Assert.IsNotNull(person2);
+            Assert.AreEqual(person.FirstName, person2.FirstName);
+            Assert.AreEqual(person.BusinessEntityId, person2.BusinessEntityId);
+        }
+    }
+}
