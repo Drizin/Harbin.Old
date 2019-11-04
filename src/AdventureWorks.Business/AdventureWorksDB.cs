cb971de03fba2220e52e11a273d6d92de98c0433
Rick Drizin <Drizin@users.noreply.github.com>
2019-11-03 21:37:27 -0500

    Harbin.Common.csproj: common infrastructure used by all other layers AdventureWorks.Business.csproj: Added Dapper (and Dapper helpers on partial class AdventureWorksDB.cs)

---
+++ src/AdventureWorks.Business/AdventureWorksDB.cs
@@ -0,0 +1,143 @@
+﻿using Dapper;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DataException = Harbin.Common.Database.DataException;
+
+namespace AdventureWorks.Business.Entities
+{
+    partial class AdventureWorksDB
+    {
+        #region CreateConnection
+
+        /// <summary>
+        /// Creates a raw SqlConnection to the database. This is NOT for using Entity Framework, but Dapper or raw ADO.NET.
+        /// Don't forget to wrap your connection inside a "using" statement, to automatically close/dispose connection at end:
+        /// using (var cn = new AdventureWorksDB().CreateConnection())
+        /// {
+        ///    ...
+        /// }
+        /// </summary>
+        /// <returns></returns>
+        public System.Data.SqlClient.SqlConnection CreateConnection()
+        {
+            string cnStr = this.Database.Connection.ConnectionString;
+            var cn = new System.Data.SqlClient.SqlConnection(cnStr);
+            cn.Open();
+            return cn;
+        }
+        #endregion
+
+        #region Dapper Wrappers
+        /// <summary>
+        /// Execute parameterized SQL
+        /// </summary>
+        /// <returns>Number of rows affected</returns>
+        public int Execute(string sql, object parms = null, IDbTransaction transaction = null, int? commandTimeout = default(int?), CommandType? commandType = default(CommandType?))
+        {
+            using (var cn = this.CreateConnection())
+            {
+                //DB.SetConnectionUser(cn, sql);
+                try
+                {
+                    return cn.Execute(sql, parms, transaction, commandTimeout, commandType);
+                }
+                catch (System.Data.SqlClient.SqlException ex)
+                {
+                    throw DataException.WrapSqlException(ex, sql, parms);
+                }
+            }
+        }
+        /// <summary>
+        ///  Executes a query, returning the data typed as per T
+        /// </summary>
+        /// <returns>
+        /// A sequence of data of the supplied type; if a basic type (int, string, etc) is
+        ///  queried then the data from the first column in assumed, otherwise an instance
+        ///  is created per row, and a direct column-name===member-name mapping is assumed
+        ///  (case insensitive).
+        /// </returns>
+        public IEnumerable<T> Query<T>(string sql, object parms = null, IDbTransaction transaction = null, bool buffered = true, int? commandTimeout = default(int?), CommandType? commandType = default(CommandType?))
+        {
+            using (var cn = this.CreateConnection())
+            {
+                //DB.SetConnectionUser(cn, sql);
+                try
+                {
+                    return cn.Query<T>(sql, parms, transaction, buffered, commandTimeout, commandType);
+                }
+                catch (System.Data.SqlClient.SqlException ex)
+                {
+                    throw DataException.WrapSqlException(ex, sql, parms);
+                }
+            }
+        }
+
+        /// <summary>
+        ///  Return a sequence of dynamic objects      <para /> 
+        ///  Example:
+        ///  var students = DB.Query("SELECT * FROM Students");
+        ///  foreach(var student in students) Response.WriteLine(student.FirstName);  <para /> 
+        ///  var enrollments = DB.Query("SELECT * FROM Enrollments WHERE StudentIndex=@sid", new { sid = 1 } );  etc..
+        /// </summary>
+        public IEnumerable<dynamic> Query(string sql, object parms = null, IDbTransaction transaction = null, bool buffered = true, int? commandTimeout = default(int?), CommandType? commandType = default(CommandType?))
+        {
+            using (var cn = this.CreateConnection())
+            {
+                //DB.SetConnectionUser(cn, sql);
+                try
+                {
+                    return cn.Query(sql, parms, transaction, buffered, commandTimeout, commandType);
+                }
+                catch (System.Data.SqlClient.SqlException ex)
+                {
+                    throw DataException.WrapSqlException(ex, sql, parms);
+                }
+            }
+        }
+
+
+
+        /// <summary>
+        /// Inserts a record into the table and returns a single identity key generated for the insert. <para /> 
+        /// Usage: 
+        /// int userIndex = DB.Insert("INSERT INTO Users (UserName, FirstName, LastName) VALUES (@UserName, @FirstName, @LastName)", new { UserName="etc", FirstName="etc", LastName="etc"  });          <para /> 
+        /// The @@IDENTITY (actually SCOPE_IDENTITY()) will be automatically added.
+        /// </summary>
+        /// <returns></returns>
+        public int Insert(string sql, object parms, IDbTransaction transaction = null, bool buffered = true, int? commandTimeout = default(int?), CommandType? commandType = default(CommandType?))
+        {
+            using (var conn = this.CreateConnection())
+            {
+                //DB.SetConnectionUser(cn, sql);
+                try
+                {
+                    return conn.Query<int>(sql + "; SELECT SCOPE_IDENTITY();", parms, transaction, buffered, commandTimeout, commandType).Single();
+                }
+                catch (System.Data.SqlClient.SqlException ex)
+                {
+                    throw DataException.WrapSqlException(ex, sql, parms);
+                }
+            }
+        }
+
+        /// <summary>
+        /// Updates one or more records, and returns the number of affected records.          <para />
+        /// Usage: 
+        /// int updatedRecords = DB.Update("UPDATE Users SET UserName=@UserName WHERE UserIndex=@UserIndex", new { UserName="etc", UserIndex=1  });
+        /// </summary>
+        public int Update(string sql, object parms, IDbTransaction transaction = null, bool buffered = true, int? commandTimeout = default(int?), CommandType? commandType = default(CommandType?))
+        {
+            return this.Execute(sql, parms, transaction, commandTimeout, commandType);
+        }
+
+
+        #endregion
+
+    }
+
+
+}
---
+++ src/Harbin.Common/Database/DataException.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Harbin.Common.Database
+{
+
+    #region DataException
+    public class DataException : System.Exception
+    {
+        #region Members
+        public string SqlQuery { get; set; }
+        public object Parms { get; set; }
+        #endregion
+        protected DataException(string message, Exception innerException, string sqlQuery = null, object parms = null) : base(message, innerException)
+        {
+        }
+
+        /// <summary>
+        /// Wraps the SqlException, add contextual information (SqlQuery, parameter values), and then you should rethrow (do not swallow/ignore exceptions)
+        /// According to the SQL error code, we can create a more meaningful message (parsing information from the DB error), which can later be translated by i18n
+        /// </summary>
+        /// <param name="ex"></param>
+        public static DataException WrapSqlException(System.Data.SqlClient.SqlException ex, string sqlQuery = null, object parms = null)
+        {
+            // By default we keep the original SQL message
+            string message = ex.Message;
+
+            // According to the SQL error code, we can create a more meaningful message - see fields in https://stackoverflow.com/a/22126876/3606250 and codes in ...
+            switch (ex.Errors[0].Number)
+            {
+                case 8134:
+                    {
+                        var match = System.Text.RegularExpressions.Regex.Match(ex.Message, "Msg 17001, Violation of Unique Key (<Constraint>)");
+                        string constraint = match.Groups["Constraint"].Value;
+                        //message = string.Format("[[[VIOLATION_UNIQUE_INDEX|||(((constraint)))]]"); // throw error with i18n-translatable message
+                        //this.ViolatedConstraint = constraint //TODO: save this in logs - DataException.SaveToLog?
+                        break;
+                    }
+                default:
+                    break;
+            }
+
+            DataException wrappedException = new DataException(null, ex, sqlQuery, parms);
+            return wrappedException;
+        }
+    }
+
+    #endregion
+}
