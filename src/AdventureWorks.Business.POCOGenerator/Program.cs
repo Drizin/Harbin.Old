29af5906f0429c8bddaf7b69b6eda40c51b333a9
Rick Drizin <Drizin@users.noreply.github.com>
2019-11-03 22:45:02 -0500

    AdventureWorks.Business.POCOGenerator is a code generation template using CodegenCS - reads AdventureWorks SQL Server Database and generates a DbContext and POCOS for Entity Framework 6: This is a fresh copy of https://github.com/Drizin/CodegenCS/tree/master/src/Templates/EF6-POCO-Generator , which in case is based on https://github.com/sjh37/EntityFramework-Reverse-POCO-Code-First-Generator/

--- src/AdventureWorks.Business.POCOGenerator/Program.cs
+++ src/AdventureWorks.Business.POCOGenerator/Program.cs
@@ -1,8 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using CodegenCS;
+using System;
+using System.IO;
 
 namespace AdventureWorks.Business.POCOGenerator
 {
@@ -10,6 +8,29 @@ namespace AdventureWorks.Business.POCOGenerator
     {
         static void Main(string[] args)
         {
+            string projectFolder = System.IO.Path.Combine(CodegenCS.Utils.IO.GetCurrentDirectory().FullName, @"..\AdventureWorks.Business");
+            CodegenContext context = new CodegenContext(outputFolder: projectFolder + "\\GeneratedCode");
+            Generator generator = new Generator(
+                context: context,
+                createConnection: () => new System.Data.SqlClient.SqlConnection(@"
+                    Data Source=LENOVOFLEX5\SQLEXPRESS;
+                    Initial Catalog=AdventureWorks;
+                    Integrated Security=True;
+                    Application Name=EntityFramework POCO Generator"
+                ),
+                targetFrameworkVersion: 4.72m
+                );
+            generator.GenerateMultipleFiles(); // generates in memory
+
+            // since no errors, first modify csproj, then we save all files
+
+            // Generate all files and add each into to the csproj
+            MSBuildProjectEditor editor = new MSBuildProjectEditor(projectFilePath: projectFolder + @"\AdventureWorks.Business.csproj");
+            //string templateFile = Path.Combine(CodegenCS.Utils.IO.GetCurrentDirectory().FullName);
+            foreach (var o in context.OutputFilesAbsolute)
+                editor.AddItem(itemPath: o.Key, itemType: o.Value.ItemType);
+            editor.Save();
+            context.SaveFiles(deleteOtherFiles: true);
         }
     }
 }
